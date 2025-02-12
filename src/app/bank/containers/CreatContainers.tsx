c32780128c67a8a5014e47d4ae16d2b6f6cbebcf
JINJU PARK <aricclunj@naver.com>
2025-02-12 11:41:34 +0900

    수정

---
+++ src/app/bank/containers/CreatContainers.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+// import React, { useState, useCallback, useActionState } from 'react'
+// import { useSearchParams } from 'next/navigation'
+// import JoinForm from '../components/JoinForm'
+// import { processJoin } from '../services/actions'
+import React from 'react'
+
+const CreatContainer = () => {
+  return <></>
+}
+
+export default React.memo(CreatContainer)
