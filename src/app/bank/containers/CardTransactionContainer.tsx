f7d4c94fcd5280d2a878701dd3873f9b348aa202
kks940104 <kks940104@gmail.com>
2025-02-16 12:54:47 +0900

    빌드완료

---
+++ src/app/bank/containers/CardTransactionContainer.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useState, useActionState, useCallback } from 'react'
+import { CardTransaction } from '../services/actions'
+import CardTransactionForm from '../components/CardTransactionForm'
+
+const TransactionContainer = () => {
+  // 1. 자신의 계좌를 통틀어서 해야함
+  // 2. 받아야 할건 연회비, 카드종류.
+  // 3. 선택하고 난 후에 페이지 이동은 하지말자. 바로 밑에 카드를 넣자.
+  // 4. 합치지말자
+
+  /**
+   * 이렇게만 있으면 될듯...?
+   * 이부분들이 계좌 연동.
+   */
+  const [form, setForm] = useState({})
+
+  const actionState = useActionState(CardTransaction, undefined)
+  // if (title === 'card') actionState
+  // // 여기에 Card action 넣어주면 될듯
+  // else actionState = useActionState(LoanTransaction, undefined)
+
+  const onChange = useCallback((e) => {
+    setForm((form) => ({ ...form, [e.target.name]: e.target.value }))
+  }, [])
+
+  // 내 카드추천목록들.
+  const [item, setItem] = useState({})
+
+  // const itemChagne = useCallback((item) => {
+  //   setItem()
+  // })
+
+  return (
+    <>
+      <CardTransactionForm
+        form={form}
+        actionState={actionState}
+        onChange={onChange}
+      />
+    </>
+  )
+}
+
+export default React.memo(TransactionContainer)
