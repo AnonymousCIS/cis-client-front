397ace948bde76c77638a58f69324159a258c36f
JINJU PARK <aricclunj@naver.com>
2025-02-17 19:48:39 +0900

    .

--- src/app/mypage/containers/MyPageContainer.tsx
+++ src/app/mypage/containers/MyPageContainer.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import WithUserContainer from '@/app/global/containers/WithUserContainer'
+import { MainTitle } from '@/app/global/components/StyledTitle'
 
 const MypageContainer = () => {
-  return <h1>마이 페이지</h1>
+  return WithUserContainer(
+    //<h1>마이 페이지</h1>
+
+    <>
+      <MainTitle></MainTitle>
+      <MypageContainer />
+    </>,
+  )
 }
 
 export default React.memo(MypageContainer)
