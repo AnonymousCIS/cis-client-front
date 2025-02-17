233b54fff6013a8b9e64f2722a60ccfcf722e9d6
PaB1010 <mudodd@naver.com>
2025-02-17 09:47:36 +0900

    fix

--- src/app/main/containers/MainContainer.tsx
+++ src/app/main/containers/MainContainer.tsx
@@ -1,10 +1,39 @@
 'use client'
 
 import React from 'react'
-import MainForm from '../components/MainForm'
+import styled from 'styled-components'
+
+import colors from '@/app/global/styles/colors'
+import sizes from '@/app/global/styles/sizes'
+
+const { info } = colors
+
+const StyledContent = styled.div`
+  background: ${info};
+
+  .layout-width {
+    // display: flex;
+  }
+`
 
 const MainContainer = () => {
-  return <MainForm />
+  return (
+    <>
+      <StyledContent>
+        <div className="layout-width">
+          <h1>
+            ✨ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡlayout-widthㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ✨
+          </h1>
+          <h1>globals.css의 .layout-width 꼭 참고 (메인 컨텐츠 영역)</h1>
+          <h1>
+            모든 Container은 가장 상위 태그에 div태그로 className="layout-width"
+            붙일 것
+          </h1>
+          <h1>예시 - Card/ListContainer</h1>
+        </div>
+      </StyledContent>
+    </>
+  )
 }
 
 export default React.memo(MainContainer)
